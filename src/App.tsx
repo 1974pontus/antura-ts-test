d549551c26ce64e642cfddf40e3eb6bb3b426247
Pontus Tahr <pontus.tahir@gmail.com>
2021-09-01 15:44:44 +0200

    ts first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [users, setUsers] = useState<[]>([]);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //fetch
+  const getUserData = async () => {
+    setIsLoading(true);
+    const res = await fetch("https://randomuser.me/api");
+    if (res.status >= 200 && res.status <= 299) {
+      const data = await res.json();
+      setUsers(data.results);
+      setErrorMessage(false);
+      setIsLoading(false);
+    } else {
+      console.log(res.status, res.statusText);
+      setErrorMessage(true);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  function handleClick() {
+    getUserData();
+  }
+
+  if (errorMessage) {
+    return <h3>something broke</h3>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random User!</h1>
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div className="card">
+          {users.map((user) => {
+            const {
+              login: { uuid },
+              name: { first, last },
+              email,
+              picture: { medium },
+            } = user;
+            return (
+              <div key={uuid}>
+                <img className="pic" src={medium} alt="user img" />
+                <p>
+                  {first} {last}
+                </p>
+                <p>{email}</p>
+              </div>
+            );
+          })}
+          <button onClick={handleClick}>Get me a new user</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
