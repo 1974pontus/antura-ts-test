4618828470103a4db1aadbf45c68e820295c1e90
Pontus Tahir <55481590+1974pontus@users.noreply.github.com>
2021-09-02 11:39:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import Person from "./components/Person";
 
 const App: React.FC = () => {
   const [users, setUsers] = useState<[]>([]);
   const [errorMessage, setErrorMessage] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  //fetch
   const getUserData = async () => {
     setIsLoading(true);
     const res = await fetch("https://randomuser.me/api");
@@ -25,41 +25,45 @@ const App: React.FC = () => {
     getUserData();
   }, []);
 
-  function handleClick() {
-    getUserData();
-  }
-
   if (errorMessage) {
     return <h3>something broke</h3>;
   }
 
   return (
     <div className="App">
-      <h1>Random User!</h1>
-      {isLoading ? (
-        <p>Loading ...</p>
-      ) : (
-        <div className="card">
-          {users.map((user) => {
-            const {
-              login: { uuid },
-              name: { first, last },
-              email,
-              picture: { medium },
-            } = user;
-            return (
-              <div key={uuid}>
-                <img className="pic" src={medium} alt="user img" />
-                <p>
-                  {first} {last}
-                </p>
-                <p>{email}</p>
-              </div>
-            );
-          })}
-          <button onClick={handleClick}>Get me a new user</button>
-        </div>
-      )}
+      <div className="card">
+        <h1>Random User!</h1>
+        {isLoading ? (
+          <p>Loading ...</p>
+        ) : (
+          <div className="content">
+            {users.map((user) => {
+              const {
+                login: { uuid },
+                name: { first, last },
+                picture: { medium },
+                location: { country, state },
+                email,
+                cell,
+              } = user;
+              return (
+                <div key={uuid}>
+                  <Person
+                    medium={medium}
+                    email={email}
+                    cell={cell}
+                    first={first}
+                    last={last}
+                    country={country}
+                    getUserData={getUserData}
+                    state={state}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
